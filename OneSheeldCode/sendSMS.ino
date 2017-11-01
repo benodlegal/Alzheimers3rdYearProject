acea028f274ccc3380f6fe47207b2c653fd0a503
Calvin Riordan <calvinriordan1997@gmail.com>
2017-11-01 21:33:23 +0000

    Add files via upload

---
+++ OneSheeldCode/sendSMS.ino
@@ -0,0 +1,22 @@
+#define CUSTOM_SETTINGS 
+#define INCLUDE_SMS_SHIELD
+#define INCLUDE_TERMINAL_SHIELD
+
+#include <OneSheeld.h>
+int x = 0;
+void setup() {
+  // put your setup code here, to run once:
+  OneSheeld.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  String number= "0851933899" ;
+  // chris phone num -> String number= "0864530479" ;
+  String message= "are you still having fun" ;
+
+  Terminal.print(message);
+  SMS.send(number,message);
+  x++;
+  
+}
